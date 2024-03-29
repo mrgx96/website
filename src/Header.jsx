d4d39f2052e3eb3ca819d09787b1befe5f4b6bf2
Malek Sharabi <m.malek1996@hotmail.com>
2024-03-29 12:02:54 +0000

    Update icon & header

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <h1 className="header">
       <img
-        className="Logo"
+        className="logo"
         src="https://cdn.discordapp.com/attachments/1117557234461847723/1222307219471732816/banner7.png?ex=6615bd4e&is=6603484e&hm=6b9f79ea049da0d124e6f8b56cedfd0a089e36f38674a0e3ce69df8711c29eed&"
         alt="Logo"
       />
