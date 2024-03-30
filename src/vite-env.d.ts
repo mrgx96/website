26fee509ad659e529488eac15e71ca253ae2d555
mirror0000 <poxii2001@gmail.com>
2024-03-30 09:12:28 -0700

    fix: timer & progress bar

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,8 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_COUNTDOWN_SINCE: string;
+  readonly VITE_SALE_DATE_SINCE: string;
+  readonly VITE_SALE_DATE_UNTIL: string;
   readonly VITE_WALLET_ADDRESS: string;
   readonly VITE_WALLET_QR_CODE: string;
 }
