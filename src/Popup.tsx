0c04df97961e46839ba5e9c1b15fdcc1adee0fa5
Malek Sharabi <raspinglemon69@hotmail.com>
2024-03-31 03:23:37 +0200

    Final tweaks

--- src/Popup.tsx
+++ src/Popup.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCopy, faCheck } from '@fortawesome/free-solid-svg-icons';
 
 interface IPopup {
   onClose: Function;
 }
 
 export default function Popup({ onClose }: IPopup) {
+  const walletAddress = import.meta.env.VITE_WALLET_ADDRESS;
+  const [copied, setCopied] = useState(false);
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(walletAddress);
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 2000); // Reset "copied" state after 2 seconds
+  };
+
   return (
     <div className="overlay-modal" onClick={() => onClose?.call(null)}>
-      <div
-        className="modal"
-        onClick={(evt) => {
-          evt.stopPropagation();
-          evt.preventDefault();
-        }}
-
-      >
+      <div className="modal" onClick={(evt) => { evt.stopPropagation(); evt.preventDefault(); }}>
         <button className="close-button" onClick={() => onClose?.call(null)}>X</button>
         <h4>P.S. Send ONLY Solana ($SOL) to the following address</h4>
-        <div className="adressContainer">
-        <pre className="address">{import.meta.env.VITE_WALLET_ADDRESS}</pre>
+        <div className="addressContainer">
+          <div className="input-container">
+            <input type="text" value={walletAddress} readOnly />
+            <div className="icon-container" onClick={copyToClipboard}>
+              <FontAwesomeIcon icon={copied ? faCheck : faCopy} />
+            </div>
+          </div>
         </div>
         <img
           className="qr-code"
@@ -26,7 +37,6 @@ export default function Popup({ onClose }: IPopup) {
           alt="wallet address QR code"
         />
         <h4>How To Buy?</h4>
-
         <h5>Step 1: <span>Buy Solana from any exchange (Coinbase, Binance etc....)</span></h5>
         <h5>Step 2: <span>Create a Solana wallet (Phantom, Solfare etc...) and deposit your Solana into the wallet</span></h5>
         <h5>Step 3: <span>Click on "Send" and copy the address above and paste it into the field OR simply scan the QR code above from your wallet</span></h5>
